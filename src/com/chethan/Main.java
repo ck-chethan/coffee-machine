3ee477ed3193f9c2b628f4c0084ce107032b91a0
Chethan C K <chethanckm66@gmail.com>
2022-07-17 23:57:56 +0530

    Coffee Machine - 1->Making Coffee

--- src/com/chethan/Main.java
+++ src/com/chethan/Main.java
@@ -3,6 +3,12 @@ package com.chethan;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        System.out.println("Starting to make a coffee");
+        System.out.println("Grinding coffee beans");
+        System.out.println("Boiling water");
+        System.out.println("Mixing boiled water with crushed coffee beans");
+        System.out.println("Pouring coffee into the cup");
+        System.out.println("Pouring some milk into the cup");
+        System.out.println("Coffee is ready!");
     }
 }
