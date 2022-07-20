cbd16cf375d807166b0f92f461d7933fdb6ae53f
Chethan C K <chethanckm66@gmail.com>
2022-07-21 00:06:16 +0530

    Coffee Machine - 5 -> Keep track of the supplies

--- src/com/chethan/Main.java
+++ src/com/chethan/Main.java
@@ -11,40 +11,94 @@ public class Main {
     static Scanner sc = new Scanner(System.in);
 
     public static void main(String[] args) {
-        coffeeMachineStatus();
         makeAction();
-        coffeeMachineStatus();
     }
 
     public static void makeAction() {
-        System.out.println("Write action (buy, fill, take): ");
-        String actionType = sc.next();
-        switch (actionType) {
-            case "buy" -> buyAction();
-            case "fill" -> fillAction();
-            case "take" -> takeAction();
+        while (true) {
+            System.out.println("Write action (buy, fill, take, remaining, exit): ");
+            String actionType = sc.next();
+            switch (actionType) {
+                case "buy" -> buyAction();
+                case "fill" -> fillAction();
+                case "take" -> takeAction();
+                case "remaining" -> coffeeMachineStatus();
+                case "exit" -> {
+                    return;
+                }
+            }
         }
     }
 
     public static void buyAction() {
         System.out.println("What do you want to buy? 1 - espresso, 2 - latte, 3 - cappuccino: ");
-        int coffeeType = sc.nextInt();
-        if (coffeeType == 1) {
-            waterSupply -= 250;
-            coffeeBeanSupply -= 16;
-            moneyPresent += 4;
-        } else if (coffeeType == 2) {
-            waterSupply -= 350;
-            milkSupply -= 75;
-            coffeeBeanSupply -= 20;
-            moneyPresent += 7;
-        } else if (coffeeType == 3) {
-            waterSupply -= 200;
-            milkSupply -= 100;
-            coffeeBeanSupply -= 12;
-            moneyPresent += 6;
+        String coffeeType = sc.next();
+        if(coffeeType.equals("back")) {
+            return;
+        }
+        if (!canMakeRequiredCoffee(coffeeType)){
+            return;
+        }
+        switch (coffeeType) {
+            case "1" -> {
+                waterSupply -= 250;
+                coffeeBeanSupply -= 16;
+                moneyPresent += 4;
+            }
+            case "2" -> {
+                waterSupply -= 350;
+                milkSupply -= 75;
+                coffeeBeanSupply -= 20;
+                moneyPresent += 7;
+            }
+            case "3" -> {
+                waterSupply -= 200;
+                milkSupply -= 100;
+                coffeeBeanSupply -= 12;
+                moneyPresent += 6;
+            }
         }
         cupsSupply -= 1;
+        System.out.println("I have enough resources, making you a coffee! ");
+    }
+
+    public static boolean canMakeRequiredCoffee(String coffeeType) {
+        switch (coffeeType) {
+            case "1":
+                if (waterSupply < 250) {
+                    System.out.println("Sorry, not enough water! ");
+                    return false;
+                } else if (coffeeBeanSupply < 16) {
+                    System.out.println("Sorry, not enough coffee beans! ");
+                    return false;
+                }
+                break;
+            case "2":
+                if (waterSupply < 350) {
+                    System.out.println("Sorry, not enough water! ");
+                    return false;
+                } else if (milkSupply < 20) {
+                    System.out.println("Sorry, not enough coffee beans! ");
+                    return false;
+                } else if (coffeeBeanSupply < 20) {
+                    System.out.println("Sorry, not enough coffee beans! ");
+                    return false;
+                }
+                break;
+            case "3":
+                if (waterSupply < 200) {
+                    System.out.println("Sorry, not enough water! ");
+                    return false;
+                } else if (milkSupply < 100) {
+                    System.out.println("Sorry, not enough coffee beans! ");
+                    return false;
+                } else if (coffeeBeanSupply < 12) {
+                    System.out.println("Sorry, not enough coffee beans! ");
+                    return false;
+                }
+                break;
+        }
+        return true;
     }
 
     public static void fillAction() {
