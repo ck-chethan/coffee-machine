92e1978ececf6c7d5dc593d8a2c5ac752239dd10
Chethan C K <chethanckm66@gmail.com>
2022-07-20 23:00:52 +0530

    Coffee Machine - 4 -> Buy, fill, take!

--- src/com/chethan/Main.java
+++ src/com/chethan/Main.java
@@ -3,35 +3,73 @@ package com.chethan;
 import java.util.Scanner;
 
 public class Main {
+    static int waterSupply = 400;
+    static int milkSupply = 540;
+    static int coffeeBeanSupply = 120;
+    static int cupsSupply = 9;
+    static int moneyPresent = 550;
+    static Scanner sc = new Scanner(System.in);
 
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+        coffeeMachineStatus();
+        makeAction();
+        coffeeMachineStatus();
+    }
 
-//        System.out.println("Write how many cups of coffee you will need: ");
-//        int noOfCups = sc.nextInt();
-//        requirements(noOfCups);
-        System.out.println("Write how many ml of water the coffee machine has: ");
-        int waterSupply = sc.nextInt();
-        System.out.println("Write how many ml of milk the coffee machine has: ");
-        int milkSupply = sc.nextInt();
-        System.out.println("Write how many grams of coffee beans the coffee machine has:");
-        int coffeeBeansSupply = sc.nextInt();
-
-        int coffeesCanMake = noCoffeeCanMake(waterSupply, milkSupply, coffeeBeansSupply);
-
-        System.out.println("Write how many cups of coffee you will need: ");
-        int coffeeRequired = sc.nextInt();
-
-        if (coffeeRequired == coffeesCanMake) {
-            System.out.println("Yes, I can make that amount of coffee ");
-
-        } else if (coffeeRequired < coffeesCanMake) {
-            int noOfMoreCoffee = coffeesCanMake - coffeeRequired;
-            System.out.printf("Yes, I can make that amount of coffee (and even %d more than that) %n", noOfMoreCoffee);
-        } else {
-            System.out.printf("No, I can make only %d cup(s) of coffee", coffeesCanMake);
+    public static void makeAction() {
+        System.out.println("Write action (buy, fill, take): ");
+        String actionType = sc.next();
+        switch (actionType) {
+            case "buy" -> buyAction();
+            case "fill" -> fillAction();
+            case "take" -> takeAction();
         }
+    }
 
+    public static void buyAction() {
+        System.out.println("What do you want to buy? 1 - espresso, 2 - latte, 3 - cappuccino: ");
+        int coffeeType = sc.nextInt();
+        if (coffeeType == 1) {
+            waterSupply -= 250;
+            coffeeBeanSupply -= 16;
+            moneyPresent += 4;
+        } else if (coffeeType == 2) {
+            waterSupply -= 350;
+            milkSupply -= 75;
+            coffeeBeanSupply -= 20;
+            moneyPresent += 7;
+        } else if (coffeeType == 3) {
+            waterSupply -= 200;
+            milkSupply -= 100;
+            coffeeBeanSupply -= 12;
+            moneyPresent += 6;
+        }
+        cupsSupply -= 1;
+    }
+
+    public static void fillAction() {
+        System.out.println("Write how many ml of water you want to add: ");
+        waterSupply += sc.nextInt();
+        System.out.println("Write how many ml of milk you want to add: ");
+        milkSupply += sc.nextInt();
+        System.out.println("Write how many grams of coffee beans you want to add: ");
+        coffeeBeanSupply += sc.nextInt();
+        System.out.println("Write how many disposable cups of coffee you want to add: ");
+        cupsSupply += sc.nextInt();
+    }
+
+    public static void takeAction() {
+        System.out.printf("I gave you $%d %n", moneyPresent);
+        moneyPresent = 0;
+    }
+
+    public static void coffeeMachineStatus() {
+        System.out.printf("The coffee machine has: %n");
+        System.out.printf("%d ml of water %n", waterSupply);
+        System.out.printf("%d ml of milk %n", milkSupply);
+        System.out.printf("%d g of coffee beans %n", coffeeBeanSupply);
+        System.out.printf("%d disposable cups %n", cupsSupply);
+        System.out.printf("$%d of money %n", moneyPresent);
     }
 
     public static void requirements(int noOfCups) {
